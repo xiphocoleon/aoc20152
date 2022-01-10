eb9009d9363132a6430a2f801bf0c79cf78c510b
xiphocoleon <thomasedwardwilk@gmail.com>
2022-01-10 09:42:07 -0500

    Project files

---
+++ wrappingPaperCalculator/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wrappingPaperCalculator
+{
+    public class WrappingCalculations
+    {
+        int area = 0;
+        int smallestArea = 0;
+        int sum1;
+        int sum2;
+        int sum3;
+
+        public int findArea(int a, int b)
+        {
+            area = a * b;
+            return area;
+        }
+        public int findSmallestSide(int a, int b, int c)
+        {
+            sum1 = a * b;
+            sum2 = a * c;
+            sum3 = b * c; 
+
+            if(sum1 <= smallestArea)
+
+            return smallestArea;
+        }
+    }
+}
---
+++ wrappingPaperCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿int counter = 0;
+int finalSum = 0;
+int packageSum = 0;
+
+int length = 0;
+int width = 0;
+int height = 0;
+
+int smallestSide = 0;
+
+
+//System.Console.WriteLine("Test creating a class: " + wrappingPaperCalculator.Class1.testInt() );
+
+//string text = System.IO.File.ReadAllText(@"C:\Users\19084\source\repos\wrappingPaperCalculator\wrapping.txt");
+
+foreach (string line in System.IO.File.ReadLines(@"C:\Users\19084\source\repos\wrappingPaperCalculator\wrapping.txt"))
+{
+   //System.Console.WriteLine(line);
+   var boxDims = line.Split('x');
+    
+    smallestSide = Int16.Parse(boxDims[0]) * Int16.Parse(boxDims[1]);
+    if (smallestSide >= Int16.Parse(boxDims[0]) * Int16.Parse(boxDims[2]))
+        smallestSide = Int16.Parse(boxDims[0]) * Int16.Parse(boxDims[2]);
+
+
+
+
+
+
+   counter++;
+}
+
+//System.Console.WriteLine(line);
+
+
+//System.Console.WriteLine("There were {0} lines.", counter);
+// Suspend the screen.  
+System.Console.ReadLine();
